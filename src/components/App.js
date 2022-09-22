ece2e91519dd0274f8dd7ca9707e523397ab25c3
awadh <awadh.hassan@student.moringaschool.com>
2022-09-22 12:06:38 +0300

    assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,17 @@ console.log(user);
 function App() {
   return (
     <div>
+
       <NavBar />
-      <Home />
-      <About />
+
+      <Home name ={user.name} city ={user.city} color={user.color} />
+
+      <About 
+      bio ={user.bio}
+      github ={user.links.github}
+      linkedin ={user.links.linkedin}
+      />
+      
     </div>
   );
 }
